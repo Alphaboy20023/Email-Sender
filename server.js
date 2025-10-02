368bab47941a62139dc1a4e021730fa7c814d740
Alphaboy20023 <timivictor565@gmail.com>
2025-10-02 22:47:02 +0200

    changes

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://Akinola-Victor-Portfolio.netlify.app", "http://localhost:3000"], 
+  methods: ["POST"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 app.use("/api", emailRoutes);
 
